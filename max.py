cc64dd9cc918eaa4d6bdee6c90d414a9e0deb71b
Manu Labricole <manu_barriol@hotmail.fr>
2023-02-21 00:23:50 +0100

    pass return in int

--- max.py
+++ max.py
@@ -3,6 +3,26 @@
 # Ensuite Save + Run puis Save + Evaluate
 # Pour des raisons techniques, laissez une ligne blanche avant de commencer votre programme.
 
+def maximum(num1, num2):
+    if num1 > num2:
+        return int(num1)
+    elif num1 < num2:
+        return int(num2)
+    else:
+        return None
+
+
+def maximum3(num1, num2, num3):
+    return int(maximum(num1=maximum(num1=num1, num2=num2), num2=num3))
+
+
+def maximum3_input():
+    num1 = input("num 1 : ")
+    num2 = input("num 2 : ")
+    num3 = input("num 3 : ")
+
+    return int(maximum3(num1=num1, num2=num2, num3=num3))
+
 
 if __name__ == "__main__":  # NE PAS SUPPRIMER CETTE LIGNE
     # Votre programme principal ne sera pas évalué.
@@ -10,3 +30,5 @@ if __name__ == "__main__":  # NE PAS SUPPRIMER CETTE LIGNE
     # les appels de votre choix.
     # Respectez bien ce niveau d'identation.
     print("Debut du prog. principal")
+    print(maximum(3, 4))
+    print(maximum3(3, 4, 1))
