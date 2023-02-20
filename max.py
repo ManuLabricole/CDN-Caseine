1d2c11cd1cfff68bc7a910bb6e0a03b58ea7af29
Manu Labricole <manu_barriol@hotmail.fr>
2023-02-21 00:16:57 +0100

    create max.py

---
+++ max.py
@@ -0,0 +1,12 @@
+# Ecrivez votre programme ci-dessous.
+# Bouton Fullscreen pour passer en plein ecran
+# Ensuite Save + Run puis Save + Evaluate
+# Pour des raisons techniques, laissez une ligne blanche avant de commencer votre programme.
+
+
+if __name__ == "__main__":  # NE PAS SUPPRIMER CETTE LIGNE
+    # Votre programme principal ne sera pas évalué.
+    # Utilisez-le pour tester votre programme en faisant
+    # les appels de votre choix.
+    # Respectez bien ce niveau d'identation.
+    print("Debut du prog. principal")
