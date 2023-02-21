1e781823a9ba8001315394adb08da890b239de7e
Manu Labricole <manu_barriol@hotmail.fr>
2023-02-21 10:59:19 +0100

    qdd menu function typed

--- Theme_6/le_menu.py
+++ Theme_6/le_menu.py
@@ -2,7 +2,9 @@
 # Pour chacun des menus, le client peut choisir de rajouter une boisson à 4 euros. 
 # De plus, le client peut choisir un supplément fromage et/ou un supplément café, chaque supplément coûtant 1,50 euros.
 
-#     Écrire une fonction prix_menu qui prend comme argument le nom du menu (chaîne de caractères), puis deux arguments optionnels : un booléen avecBoisson dont la valeur par défaut est False, et un entier nb_supplement qui doit valoir 0 par défaut. La fonction doit renvoyer le prix du menu correspondant.
+#     Écrire une fonction prix_menu qui prend comme argument le nom du menu (chaîne de caractères), puis deux arguments optionnels : 
+# un booléen avecBoisson dont la valeur par défaut est False, et un entier nb_supplement qui doit valoir 0 par défaut. 
+# La fonction doit renvoyer le prix du menu correspondant.
 #     Écrire une fonction table_Dupont qui ne prend aucun argument et qui renvoie le prix total de l’addition pour la table des Dupont :
 #         Jacqueline a choisi seulement un menu Basique,
 #         Michel a pris un menu Gourmand avec boisson,
@@ -10,6 +12,9 @@
 #         et Antoine a choisi un menu Basique avec boisson et supplément café.
 
 #  (Exercice proposé par Aurélie Lagoutte)
+
+def prix_menu(menuName:str, avecBoisson:bool=False, nb_supplement:int=0) -> float:
+    return 
 
 if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
     # Votre programme principal ne sera pas évalué.
