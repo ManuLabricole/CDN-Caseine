d5a62645ea23637e5e2eb769e5f926de7f68347f
Manu Labricole <manu_barriol@hotmail.fr>
2023-02-21 16:06:31 +0100

    star exo pair

---
+++ Theme_7/pair.py
@@ -0,0 +1,31 @@
+# Ecrivez votre programme ci-dessous.
+# Bouton Fullscreen pour passer en plein ecran
+# Ensuite Save + Run puis Save + Evaluate
+# Pour des raisons techniques, laissez une ligne blanche avant de commencer votre programme.
+
+
+
+
+        
+        
+if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
+    # Votre programme principal ne sera pas évalué.
+    # Utilisez-le pour tester votre programme en faisant
+    # les appels de votre choix.
+    # Respectez bien ce niveau d'identation.
+    print("Debut du prog. principal")
+
+
+## Ci-dessous: copie des exemples
+## Pour avoir l'enonce en entier, ouvrez deux fenetres cote-a-cote.
+##somme_pairs([4, 7, 12, 0, 21, 5]) vaut 16 (car 16=4+12+0).
+##nb_elem_pairs([4, 7, 12, 0, 21, 5]) vaut 3 (car 4, 12 et 0 sont pairs).
+##max_pairs([4, 7, 12, 0, 21, 5]) vaut 12.
+##min_pairs([4, 7, 12, 0, 21, 5]) vaut 0
+##et min_pairs([9, 3, 1]) vaut None.
+##indice_de(12, [4, 7, 12, 0, 21, 5]) vaut 2 (car 12 est placé à l'indice 2),
+##et indice_de(6, [4, 7, 12, 0, 21, 5]) vaut None.
+##trouve_premier_pair([1, 15, 4, 7, 12, 3]) vaut  4
+##et trouve_premier_pair([1, 17, 7]) vaut  None.
+##extrait_pairs([4, 7, 12, 0, 3]) vaut  [4, 12, 0]
+##et extrait_pairs([21, 17, 3]) vaut  [ ].
