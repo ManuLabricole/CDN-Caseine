44f2309f36e10739ae45706051f5c486628d05b5
Emmanuel BARRIOL <emmanuel.barriol@Digital-Grenoble.local@dataplus-16>
2023-02-21 10:37:09 +0100

    setup exo Menu

---
+++ Theme_6/le_menu.py
@@ -0,0 +1,23 @@
+# Un restaurant possède trois menus : le Basique à 9 euros, le Gourmand à 15 euros et le Complet à 19 euros. 
+# Pour chacun des menus, le client peut choisir de rajouter une boisson à 4 euros. 
+# De plus, le client peut choisir un supplément fromage et/ou un supplément café, chaque supplément coûtant 1,50 euros.
+
+#     Écrire une fonction prix_menu qui prend comme argument le nom du menu (chaîne de caractères), puis deux arguments optionnels : un booléen avecBoisson dont la valeur par défaut est False, et un entier nb_supplement qui doit valoir 0 par défaut. La fonction doit renvoyer le prix du menu correspondant.
+#     Écrire une fonction table_Dupont qui ne prend aucun argument et qui renvoie le prix total de l’addition pour la table des Dupont :
+#         Jacqueline a choisi seulement un menu Basique,
+#         Michel a pris un menu Gourmand avec boisson,
+#         Johanna a choisi un menu Basique avec suppléments fromage et café,
+#         et Antoine a choisi un menu Basique avec boisson et supplément café.
+
+#  (Exercice proposé par Aurélie Lagoutte)
+
+if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
+    # Votre programme principal ne sera pas évalué.
+    # Utilisez-le pour tester votre programme en faisant
+    # les appels de votre choix.
+    # Respectez bien ce niveau d'identation.
+    print("Debut du prog. principal")
+
+
+
+
