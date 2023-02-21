6664a0e3394bbfa2dbb449c24d53f3ae983ec7e6
Emmanuel BARRIOL <emmanuel.barriol@Digital-Grenoble.local@dataplus-16>
2023-02-21 09:28:07 +0100

    est premier function

--- pyramide.py
+++ pyramide.py
@@ -34,6 +34,20 @@
 # Par exemple, si le nombre préféré du sphinx est 15, alors le code secret du hall ce jour-là est 17 (car 17 et 19 sont premiers). 
 # Si le nombre préféré d'Osiris ce jour-là est 20, alors le code secret de la salle du trésor est 63 (car 63+1 est une puissance de 2, et 63= 12 x 5+3).
 
+def est_premier(num:int) -> bool:
+    num = int(num)
+    if num < 2:
+        return False
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                # print("Pas premier, divisible par" + str(i))
+                return False
+
+            else:
+                pass# print("Premier, non divisible par :" + str(i))
+        return True
+
 if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
     # Votre programme principal ne sera pas évalué.
     # Utilisez-le pour tester votre programme en faisant
@@ -46,4 +60,5 @@ if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
     # Ci-dessous: programme qui simule votre entree dans la pyramide
     # Vous devez juste appuyer sur Entree lorsque l'on vous le demande.
     import pyramide
-    pyramide.entre() # Vous pouvez commenter cette ligne si vous le voulez.
+    # pyramide.entre() # Vous pouvez commenter cette ligne si vous le voulez.
+    print(est_premier(18))
