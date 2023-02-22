c2f6efad4e27ce2a57f9d90a570e652faa24a0df
Manu Labricole <manu_barriol@hotmail.fr>
2023-02-22 13:59:20 +0100

    list patient norm

--- Theme_10/write.py
+++ Theme_10/write.py
@@ -23,10 +23,54 @@ def affiche_console(nom_fichier):
 def import_lignes(nomFichier:str):
     f = open(nomFichier)
     listFile = f.readlines()
-    print(f.read())
-    print(listFile)
     f.close()
-        
+    
+    return listFile
+
+def cree_patient(ligne:str) -> dict:
+    
+    newLigne = separe(ligne)
+    
+    patientDict = {
+        "nom" : str(newLigne[0]),
+        "poids" : float(newLigne[1]),
+        "taille" : float(newLigne[2])
+    }
+    
+    return patientDict
+
+def liste_patients_from_nom_fichier(nomFichier:str) -> list:
+    rawList = import_lignes(nomFichier=nomFichier)
+    listPatient = []
+    
+    for el in rawList:
+        listPatient.append(cree_patient(el))
+    
+    return listPatient
+    
+def imc(patient:dict) -> float:
+    
+    imc = patient["poids"]/patient["taille"]/patient["taille"]
+    imc = round(imc, 2)
+    
+    return imc
+
+def imc_moyen(listPatient:list) -> float:
+    imcList = []
+    for el in listPatient:
+        imcList.append(imc(el))
+    
+    imc_moy = round(sum(imcList)/len(imcList), 2)
+    
+    return imc_moy
+
+def liste_noms_patients_en_corpulence_normale(listPatient:list) -> list:
+    listPatientNorm = []
+    for patient in listPatient:
+        if imc(patient) >= 18.5 and imc(patient) <= 25:
+            listPatientNorm.append(patient["nom"])    
+    
+    return listPatientNorm
         
 if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
     # Votre programme principal ne sera pas évalué.
@@ -37,5 +81,6 @@ if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
     
     print("Test de la fonction affiche_console")
     #affiche_console("resultats-donnees1.txt")
-    import_lignes("donnees1.txt")
+    ListPatient = import_lignes("donnees1.txt")
+    cree_patient(ListPatient[0])
    
