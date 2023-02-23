b47ec52341c73ec34d34cae9a97b22da81c65314
Manu Labricole <manu_barriol@hotmail.fr>
2023-02-23 16:36:41 +0100

    start build tuple year-arbre

--- Theme_2/guigui_marion.py
+++ Theme_2/guigui_marion.py
@@ -2,7 +2,7 @@
 # Bouton Fullscreen pour passer en plein ecran
 # Ensuite Save + Run puis Save + Evaluate
 # Pour des raisons techniques, laissez une ligne blanche avant de commencer votre programme.
-    
+
 
 print("Lancement de la gestion des comptes? ")
 answer = input()
@@ -10,37 +10,38 @@ answer = input()
 if answer == "oui":
     print("Solde du compte de Guillaume?")
     accountGui = float(input())
-    
+
     print("Solde du compte de Marion?")
     accountMar = float(input())
 
     # Both are positive
     if (accountGui > 0 and accountMar > 0):
-        print("Tous les deux en positif!")   
-        
+        print("Tous les deux en positif!")
+
     # Both are negative
     elif (accountGui < 0 and accountMar < 0):
-        print("Tous les deux en negqtif !") 
-    
+        print("Tous les deux en negatif !")
+
     # Guillaume is negative
     elif accountGui < 0 and accountMar > 0:
         print("Guillaume est en negatif")
         # Guillaume peut l aider
         if abs(accountGui) < abs(accountMar):
-            print("Marion peut lui transférer : " + str(abs(accountGui)))  
-            print("(Il lui restera :" + " " + str(abs(accountMar)-abs(accountGui)) + "euros)")
+            print("Marion peut lui transférer : " + str(abs(accountGui)))
+            print("(Il lui restera :" + " " +
+                  str(abs(accountMar)-abs(accountGui)) + "euros)")
         else:
             print("Impossible de retablir la situation")
-    
+
     elif accountMar < 0 and accountGui > 0:
         print("Marion est en negatif")
     # Guillaume peut l aider
         if abs(accountMar) < abs(accountGui):
-            print("Guillaume peut lui transférer : " + str(abs(accountMar)))  
-            print("(Il lui restera :" + " " + str(abs(accountGui)-abs(accountMar)) + "euros)")
+            print("Guillaume peut lui transférer : " + str(abs(accountMar)))
+            print("(Il lui restera :" + " " +
+                  str(abs(accountGui)-abs(accountMar)) + "euros)")
         else:
             print("Impossible de retablir la situation")
-        
 
 
 elif answer == "non":
@@ -48,4 +49,3 @@ elif answer == "non":
 
 else:
     print("Please answer by 'oui' ou 'non'")
-    
--- Theme_7_8/jeuxDeMots.py
+++ Theme_7_8/jeuxDeMots.py
@@ -4,33 +4,37 @@
 # Pour des raisons techniques, laissez une ligne blanche avant de commencer votre programme.
 
 
-def commence_par(lettre:str, mot:str) -> bool:
+def commence_par(lettre: str, mot: str) -> bool:
     listMot = list(mot)
-    
+
     if len(listMot) > 0:
+        #
         if listMot[0] == lettre:
             return True
         else:
             return False
-    
+
     else:
         raise ValueError("Empty string")
-    
-def is_voyelle(letter:str) -> bool:
+
+
+def is_voyelle(letter: str) -> bool:
     voyelleList = ["a", "e", "i", "o", "u", "y"]
-    
+
     if letter in voyelleList:
+        # return not
         return True
     else:
         return False
 
-def contient_voyelle(mot:str) -> bool:
+
+def contient_voyelle(mot: str) -> bool:
 
     listMot = list(mot)
     boolValue = False
-    
+
     for letter in listMot:
-        
+
         if is_voyelle(letter):
             boolValue = True
         else:
@@ -38,64 +42,70 @@ def contient_voyelle(mot:str) -> bool:
 
     return boolValue
 
-def get_consonne_index(mot:str) -> list:
+
+def get_consonne_index(mot: str) -> list:
     listMot = list(mot)
     indexConsonne = list()
-    
-    for i in range(0, len(listMot)):
+
+    for i in range(len(listMot)):
         if is_voyelle(listMot[i]):
             pass
         else:
             indexConsonne.append(i)
-            
+
     return indexConsonne
 
-def derniere_consonne(mot:str) -> tuple:
-    
+
+def derniere_consonne(mot: str) -> tuple:
+
     indexConsonne = get_consonne_index(mot)
-    
+
     if indexConsonne is not None:
         return max(indexConsonne), list(mot)[max(indexConsonne)]
     else:
         return None
 
-def double_consonne(mot:str):
+
+def double_consonne(mot: str):
     listMot = list(mot)
     indexConsonne = get_consonne_index(mot)
-    
-    for i in range(0, len(indexConsonne)-1):
-        if (indexConsonne[i+1]-indexConsonne[i] == 1) and listMot[indexConsonne[i+1]] == listMot[indexConsonne[i]] :
-            return True, listMot[indexConsonne[i]]
-    
-    return (False, None)
-    
-def envers(List:list) -> list: 
-  return list(reversed(List))
 
-def palindrome(mot:str) -> bool:
+    for i in range(len(indexConsonne)-1):
+        if (indexConsonne[i+1]-indexConsonne[i] == 1) and listMot[indexConsonne[i+1]] == listMot[indexConsonne[i]]:
+            return True, listMot[indexConsonne[i]]
+
+    return (False, None)
+
+
+def envers(List: list) -> list:
+    return list(reversed(List))
+
+
+def palindrome(mot: str) -> bool:
     listMot = list(mot)
-    
+
     if listMot == envers(listMot):
         return True
     else:
-        return False   
-    
-def mot_autorise(mot, listInterdit:str) -> bool:
+        return False
+
+
+def mot_autorise(mot, listInterdit: str) -> bool:
     autorise = True
-    
+
     if mot in listInterdit:
         autorise = False
-    
+
     return autorise
-        
-if __name__=="__main__": # NE PAS SUPPRIMER CETTE LIGNE
+
+
+if __name__ == "__main__":  # NE PAS SUPPRIMER CETTE LIGNE
     # Votre programme principal ne sera pas évalué.
     # Utilisez-le pour tester votre programme en faisant
     # les appels de votre choix.
     # Respectez bien ce niveau d'identation.
     print("Debut du prog. principal")
-    
+
     # Exemple de transformation d'un mot en liste
 
     print(double_consonne("reussite"))
-
